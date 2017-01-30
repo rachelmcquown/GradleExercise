83d2fdd6b05108272c302737e1fc8f5c76129733
Therese Dachille <tcdachille@gmail.com>
2017-01-30 10:30:32 -0500

    fixed lazy() test methods

--- src/test/java/IntegerSequenceTest.java
+++ src/test/java/IntegerSequenceTest.java
@@ -7,17 +7,17 @@ public class IntegerSequenceTest {
     
     @Test
     public void testLazyCaterer0() {
-        assertEquals(lazy(0),1);
+        assertEquals(IntegerSequence.lazy(0),1);
     }
     
     @Test
     public void testLazyCaterer1() {
-        assertEquals(lazy(1),2);
+        assertEquals(IntegerSequence.lazy(1),2);
     }
     
     @Test
     public void testLazyCaterer2() {
-        assertEquals(lazy(50),1276);
+        assertEquals(IntegerSequence.lazy(50),1276);
     }
     
     @Test
