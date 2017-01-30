a5ba39e1a8061a7181230b7fa5d80c6cd699bf2e
Therese Dachille <tcdachille@gmail.com>
2017-01-30 10:47:38 -0500

    fixed some stuff

--- src/test/java/IntegerSequenceTest.java
+++ src/test/java/IntegerSequenceTest.java
@@ -7,31 +7,42 @@ public class IntegerSequenceTest {
     
     @Test
     public void testLazyCaterer0() {
-        assertEquals(IntegerSequence.lazy(0),1);
+        IntegerSequence is = new IntegerSequence();
+	assertEquals(is.lazy(0),1);
     }
     
     @Test
     public void testLazyCaterer1() {
-        assertEquals(IntegerSequence.lazy(1),2);
+ 
+        IntegerSequence is = new IntegerSequence();
+        assertEquals(is.lazy(1),2);
     }
     
     @Test
     public void testLazyCaterer2() {
-        assertEquals(IntegerSequence.lazy(50),1276);
+        
+        IntegerSequence is = new IntegerSequence();
+	assertEquals(is.lazy(50),1276);
     }
     
     @Test
     public void testTriangle0() {
-        assertEquals(IntegerSequence.triangle(0), 0);
+        
+        IntegerSequence is = new IntegerSequence();
+	assertEquals(is.triangle(0), 0);
     }
     
     @Test
     public void testTriangle1() {
-        assertEquals(IntegerSequence.triangle(1), 1);
+        
+        IntegerSequence is = new IntegerSequence();
+	assertEquals(is.triangle(1), 1);
     }
     
     @Test
     public void testTriangle2() {
-        assertEquals(IntegerSequence.triangle(10), 55);
+     
+        IntegerSequence is = new IntegerSequence();
+        assertEquals(is.triangle(60), 1830);
     }
 }
