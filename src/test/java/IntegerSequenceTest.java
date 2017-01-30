330889ae8b7b04d378f523abc1b33fe80468e023
Therese Dachille <tcdachille@gmail.com>
2017-01-30 10:14:53 -0500

    adding test methods

---
+++ src/test/java/IntegerSequenceTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import IntegerSequence;
+
+public class IntegerSequenceTest {
+    
+    @Test
+    public void testLazyCaterer0() {
+
+    }
+    
+    @Test
+    public void testLazyCaterer1() {
+        
+    }
+    
+    @Test
+    public void testLazyCaterer2() {
+        
+    }
+    
+    @Test
+    public void testTriangle0() {
+        
+    }
+    
+    @Test
+    public void testTriangle1() {
+
+    }
+    
+    @Test
+    public void testTriangle2() {
+
+    }
+}
